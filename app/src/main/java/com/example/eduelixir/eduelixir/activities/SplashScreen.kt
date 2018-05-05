fc2f9f4f57b98e2741e321faa9394b1dc92a2b3b
Ankit Sharma <ankit.sharma@robosoftin.com>
2018-05-05 18:38:36 +0530

    Added: Splash Screen.

---
+++ app/src/main/java/com/example/eduelixir/eduelixir/activities/SplashScreen.kt
@@ -0,0 +1,28 @@
+package com.example.eduelixir.eduelixir.activities
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.eduelixir.eduelixir.MainActivity
+import com.example.eduelixir.eduelixir.R
+import java.util.*
+
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        addTimer()
+    }
+
+    private fun addTimer()
+    {
+        Timer().schedule( object : TimerTask(){
+            override fun run() {
+                val intent = Intent(this@SplashScreen, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }, 2000L)
+    }
+}
